4e0ee3e5041ebf7d01f19b3ed16dc79de5625d55
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-21 23:24:59 +0700

    Added walls with rigidbody and colliders, changed spaceship movement to rigidbody velocity

--- Assets/SpaceShip.cs
+++ Assets/SpaceShip.cs
@@ -5,116 +5,20 @@ using UnityEngine;
 public class SpaceShip : MonoBehaviour
 {
     public Vector3 position;
-    float SpeedX = 0;//Don't touch this
-    float SpeedY = 0;//Don't touch this
-    float MaxSpeed = 10;//This is the maximum speed that the object will achieve
-    float Acceleration = 10;//How fast will object reach a maximum speed
-    float Deceleration = 10;//How fast will object reach a speed of 0
+    public Rigidbody2D rb;
+    public float speed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKey("a")) && (SpeedX < MaxSpeed))
-		{
-            SpeedX = SpeedX - Acceleration * Time.deltaTime;
-            if (SpeedY > Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedY < -Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY + Deceleration * Time.deltaTime;
-            }
-            else
-            {
-                SpeedY = 0;
-            }
-
-        }
-        else if ((Input.GetKey("d")) && (SpeedX > -MaxSpeed))
-		{
-            SpeedX = SpeedX + Acceleration * Time.deltaTime;
-            if (SpeedY > Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedY < -Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY + Deceleration * Time.deltaTime;
-            }
-            else
-            {
-                SpeedY = 0;
-            }
-        }
-        else if ((Input.GetKey("w")) && (SpeedY > -MaxSpeed))
-        {
-            SpeedY = SpeedY + Acceleration * Time.deltaTime;
-            if (SpeedX > Deceleration * Time.deltaTime)
-            {
-                SpeedX = SpeedX - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedX < -Deceleration * Time.deltaTime)
-            {
-                SpeedX = SpeedX + Deceleration * Time.deltaTime;
-            }
-            else
-            {
-                SpeedX = 0;
-            }
-        }
-        else if ((Input.GetKey("s")) && (SpeedY < MaxSpeed))
-        {
-            SpeedY = SpeedY - Acceleration * Time.deltaTime;
-            if (SpeedX > Deceleration * Time.deltaTime)
-            {
-                SpeedX = SpeedX - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedX < -Deceleration * Time.deltaTime)
-            {
-                SpeedX = SpeedX + Deceleration * Time.deltaTime;
-            }
-            else
-            {
-                SpeedX = 0;
-            }
-        }
-        else
-        {
-            if (SpeedX > Deceleration * Time.deltaTime)
-			{
-                SpeedX = SpeedX - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedX < -Deceleration * Time.deltaTime)
-			{
-                SpeedX = SpeedX + Deceleration * Time.deltaTime;
-            }
-            else
-			{
-                SpeedX = 0;
-            }
-
-            if (SpeedY > Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY - Deceleration * Time.deltaTime;
-            }
-            else if (SpeedY < -Deceleration * Time.deltaTime)
-            {
-                SpeedY = SpeedY + Deceleration * Time.deltaTime;
-            }
-            else
-            {
-                SpeedY = 0;
-            }
-        }
-        position.x = transform.position.x + SpeedX * Time.deltaTime;
-        position.y = transform.position.y + SpeedY * Time.deltaTime;
-        transform.position = position;
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
     }
 }
