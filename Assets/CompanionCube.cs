54ed944a4cc29fc5b5b929c5de5994f1a6269287
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-22 11:18:29 +0700

    Added companion cube with bounce physics

---
+++ Assets/CompanionCube.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionCube : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(new Vector2(500f, 500f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        float bounce = 500f; //amount of force to apply
+        rb.AddForce(collision.contacts[0].normal * bounce);
+        rb.MoveRotation(collision.contacts[0].normalImpulse);
+    }
+}
