52dd44b0c03529aaec9774c64e7b284a8ab86018
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2021-12-23 18:48:08 +0700

    Wave System Done

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,34 +7,26 @@ public class Enemy : MonoBehaviour
     public float speed;
     public float stoppingDistance;
 
-    private readonly float directionChangeTime = 3f;
-
-    private float latestDirectionChangeTime;
+    public WaveSpawner waveSpawner;
     public float retreatDistance;
-
-
-    private Vector2 movementDirection;
-    private Vector2 movementPerSecond;
     public GameObject projectile;
     public Rigidbody2D rb;
     public Transform player;
 
-    float angle;
+    public AudioSource audioSource;
+    public AudioClip shootingAudioClip;
+
     public float startTimeBetweenShots;
     private float timeBetweenShots;
     // Start is called before the first frame update
     void Start()
     {
-
-
-        // latestDirectionChangeTime = 0f;
-        // calcuateNewMovementVector();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timeBetweenShots = startTimeBetweenShots;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         //Kalau jauh maka musuh akan mendekat spaceship
         if(Vector2.Distance(transform.position, player.position) > stoppingDistance){
@@ -46,17 +38,6 @@ public class Enemy : MonoBehaviour
         }else if(Vector2.Distance(transform.position, player.position) < retreatDistance){
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
-
-        
-
-        // if (Time.time - latestDirectionChangeTime > directionChangeTime)
-        // {
-        //     latestDirectionChangeTime = Time.time;
-        //     calcuateNewMovementVector();
-        // }
-
-        // transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
-        // transform.position.y + (movementPerSecond.y * Time.deltaTime));
 
         if (timeBetweenShots <= 0)
         {
@@ -73,14 +54,15 @@ public class Enemy : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         rb.MoveRotation(collision.contacts[0].normalImpulse);
+
+        if (collision.gameObject.tag == "Peluru")
+		{
+            Destroy(gameObject);
+
+            waveSpawner.AddDeath();
+        }
     }
 
-    void calcuateNewMovementVector()
-    {
-        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
-        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
-        movementPerSecond = movementDirection * speed;
-    }
 
     
 
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WaveSpawner : MonoBehaviour
+{
+
+    public GameObject Enemy;
+    private int wave = 1;
+    
+    public Text waveHUD;
+    
+
+    float randX;
+
+    float randY;
+    private int counterDeath;
+
+    Vector2 whereToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randX = Random.Range(-8.0f, 8.0f);
+        randY = Random.Range(-4.0f, 4.0f);
+        whereToSpawn = new Vector2(randX, randY);
+        var enemy = Instantiate(Enemy, whereToSpawn, Quaternion.identity);
+        enemy.GetComponent<Enemy>().waveSpawner = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddDeath()
+    {
+        //Nambah satu mati
+        counterDeath++;
+        //jumlah mati >= level ? 
+        if (counterDeath >= wave)
+        {
+
+            
+            wave++;
+            waveHUD.text = "Wave: " + wave;
+            for (int i = 0; i < wave; i++)
+            {
+                randX = Random.Range(-8.0f, 8.0f);
+                randY = Random.Range(-4.0f, 4.0f);
+                whereToSpawn = new Vector2(randX, randY);
+                var enemy = Instantiate(Enemy, whereToSpawn, Quaternion.identity);
+                enemy.GetComponent<Enemy>().waveSpawner = this;
+            }
+            counterDeath = 0;
+        }
+    }
+}
