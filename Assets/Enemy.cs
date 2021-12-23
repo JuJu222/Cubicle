3187d6040e729fd79e415e302cbd972f89fac9a1
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2021-12-23 12:29:01 +0700

    Newetst

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,8 +6,15 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public float stoppingDistance;
+
+    private readonly float directionChangeTime = 3f;
+
+    private float latestDirectionChangeTime;
     public float retreatDistance;
 
+
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
     public GameObject projectile;
     public Rigidbody2D rb;
     public Transform player;
@@ -18,13 +25,16 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        // latestDirectionChangeTime = 0f;
+        // calcuateNewMovementVector();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timeBetweenShots = startTimeBetweenShots;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Kalau jauh maka musuh akan mendekat spaceship
         if(Vector2.Distance(transform.position, player.position) > stoppingDistance){
@@ -32,26 +42,47 @@ public class Enemy : MonoBehaviour
         //Jika tidak terlalu deket maka musuh akan diam ditempat dan nembak
         }else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance){
             transform.position = this.transform.position;
-        
+
         }else if(Vector2.Distance(transform.position, player.position) < retreatDistance){
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
+
         
-        if(timeBetweenShots <= 0){
+
+        // if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        // {
+        //     latestDirectionChangeTime = Time.time;
+        //     calcuateNewMovementVector();
+        // }
+
+        // transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+        // transform.position.y + (movementPerSecond.y * Time.deltaTime));
+
+        if (timeBetweenShots <= 0)
+        {
             Instantiate(projectile, transform.position, Quaternion.identity);
             timeBetweenShots = startTimeBetweenShots;
-        }else{
+        }
+        else
+        {
             timeBetweenShots -= Time.deltaTime;
         }
-        
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        float bounce = 100f; //amount of force to apply
-        rb.AddForce(collision.contacts[0].normal * bounce);
         rb.MoveRotation(collision.contacts[0].normalImpulse);
     }
 
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * speed;
+    }
+
     
+
+
 }
